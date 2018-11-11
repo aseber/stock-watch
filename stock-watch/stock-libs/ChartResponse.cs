71b545268fe472514407adea94e80662fe52123b
Austin Seber <aseber@techsouth.cc>
2018-11-11 09:54:10 -0800

    Initial work getting responses from the api

---
+++ stock-watch/stock-libs/ChartResponse.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace stock_libs
+{
+    public class ChartResponse
+    {
+        /// <summary>
+        ///     The time at which this quote was captured from the market.
+        /// </summary>
+        [JsonIgnore]
+        public DateTime Timestamp => GetTimestamp(_dateComponent, _timeComponent);
+
+        [JsonProperty(PropertyName = "date", NullValueHandling = NullValueHandling.Ignore)]
+        private string _dateComponent { get; set; }
+
+        [JsonProperty(PropertyName = "minute", NullValueHandling = NullValueHandling.Ignore)]
+        private string _timeComponent { get; set; }
+
+        [JsonProperty(PropertyName = "label", NullValueHandling = NullValueHandling.Ignore)]
+        private string label { get; set; }
+
+        [JsonProperty(PropertyName = "high", NullValueHandling = NullValueHandling.Ignore)]
+        public double High { get; set; }
+
+        [JsonProperty(PropertyName = "low", NullValueHandling = NullValueHandling.Ignore)]
+        public double Low { get; set; }
+
+        [JsonProperty(PropertyName = "average", NullValueHandling = NullValueHandling.Ignore)]
+        public double Average { get; set; }
+
+        [JsonProperty(PropertyName = "volume", NullValueHandling = NullValueHandling.Ignore)]
+        public int Volume { get; set; }
+
+        [JsonProperty(PropertyName = "notional", NullValueHandling = NullValueHandling.Ignore)]
+        public double Notional { get; set; }
+
+        [JsonProperty(PropertyName = "numberOfTrades", NullValueHandling = NullValueHandling.Ignore)]
+        public int NumberOfTrades { get; set; }
+
+        [JsonProperty(PropertyName = "marketHigh", NullValueHandling = NullValueHandling.Ignore)]
+        public double MarketHigh { get; set; }
+
+        [JsonProperty(PropertyName = "marketLow", NullValueHandling = NullValueHandling.Ignore)]
+        public double MarketLow { get; set; }
+
+        [JsonProperty(PropertyName = "marketAverage", NullValueHandling = NullValueHandling.Ignore)]
+        public double MarketAverage { get; set; }
+
+        [JsonProperty(PropertyName = "marketVolume", NullValueHandling = NullValueHandling.Ignore)]
+        public int MarketVolume { get; set; }
+
+        [JsonProperty(PropertyName = "marketNotional", NullValueHandling = NullValueHandling.Ignore)]
+        public double MarketNotional { get; set; }
+
+        [JsonProperty(PropertyName = "marketNumberOfTrades", NullValueHandling = NullValueHandling.Ignore)]
+        public int MarketNumberOfTrades { get; set; }
+
+        [JsonProperty(PropertyName = "open", NullValueHandling = NullValueHandling.Ignore)]
+        public double Open { get; set; }
+
+        [JsonProperty(PropertyName = "close", NullValueHandling = NullValueHandling.Ignore)]
+        public double Close { get; set; }
+
+        [JsonProperty(PropertyName = "marketOpen", NullValueHandling = NullValueHandling.Ignore)]
+        public double MarketOpen { get; set; }
+
+        [JsonProperty(PropertyName = "marketClose", NullValueHandling = NullValueHandling.Ignore)]
+        public double MarketClose { get; set; }
+
+        [JsonProperty(PropertyName = "changeOverTime", NullValueHandling = NullValueHandling.Ignore)]
+        public double? ChangeOverTime { get; set; }
+
+        [JsonProperty(PropertyName = "marketChangeOverTime", NullValueHandling = NullValueHandling.Ignore)]
+        public double MarketChangeOverTime { get; set; }
+
+        private DateTime GetTimestamp(string date, string time)
+        {
+            var formattedDate = $"{date.Substring(0, 4)} {date.Substring(4, 2)} {date.Substring(6, 2)}";
+            return DateTime.Parse($"{formattedDate} {time}");
+        }
+    }
+}
---
+++ stock-watch/stock-libs/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stock_libs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var day = DateTime.Now.AddDays(-5);
+            var timespan = TimeSpan.FromDays(5);
+
+            var results = QueryTickerResult(client, "msft", day, timespan).Result;
+
+            foreach (var result in results)
+            {
+                Console.WriteLine($"Timestamp: {result.Timestamp} - # Trades: {result.MarketAverage}");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static async Task<IList<ChartResponse>> QueryTickerResult(HttpClient client, string ticker, DateTime startDate, TimeSpan? timespan = null)
+        {
+            var results = new List<ChartResponse>();
+            var endDate = startDate.Add(timespan ?? new TimeSpan());
+
+            do
+            {
+                var dayString = startDate.ToString("yyyyMMdd");
+                startDate = startDate.AddDays(1);
+
+                var endpoint = $"https://api.iextrading.com/1.0/stock/{ticker}/chart/date/{dayString}";
+
+                try
+                {
+                    var response = await client.GetStringAsync(endpoint);
+
+                    results.AddRange(JsonConvert.DeserializeObject<List<ChartResponse>>(response));
+                }
+                catch (Exception e)
+                {
+                    Trace.WriteLine($"Exception encountered sending a GET request: {e.StackTrace}");
+
+                    return Enumerable.Empty<ChartResponse>().ToList();
+                }
+            }
+            while (startDate <= endDate);
+
+            return results;
+        }
+    }
+}
